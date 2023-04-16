12fe3ed24d6f1c0a97101bf3cef290b08b82d2be
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-16 21:14:40 +0200

    mapStyles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,6 +4,7 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import  LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles';
+import mapStyles from './mapStyles';
 
 const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
    const classes = useStyles();
@@ -18,7 +19,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }
          center={coordinates}
          defaultZoom={14}
          margin={[50, 50, 50, 50]}
-         options={''}
+         options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
          onChange={(e) => { 
             console.log('event', e);
             setCoordinates({ lat: e.center.lat, lng: e.center.lng }); 
