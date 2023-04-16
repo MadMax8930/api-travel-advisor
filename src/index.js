c4ea429bca4f098e5f25ee690dbae39185146d2d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-16 17:17:01 +0200

    setup app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(<App />);
