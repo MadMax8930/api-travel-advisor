49aa2dfb461064c0244dbf1b206b7ecb89a219a5
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-16 13:51:04 +0200

    header & styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import useStyles from './styles';
 
 const Map = () => {
+   const classes = useStyles();
+
   return (
     <h1>Map</h1>
   );
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import useStyles from './styles';
 
 const PlaceDetails = () => {
+   const classes = useStyles();
+
   return (
     <h1>PlaceDetails</h1>
   );
