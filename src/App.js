d58f95bac2cf72432dc4d9056ae9f99e3880512d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-16 20:21:03 +0200

    api filtering

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const App = () => {
    const [coordinates, setCoordinates] = useState({});
    const [bounds, setBounds] = useState({});
    const [isLoading, setIsLoading] = useState(false);
+   const [type, setType] = useState('restaurants');
+   const [rating, setRating] = useState('');
 
    useEffect(() => {
       navigator.geolocation.getCurrentPosition(( { coords: { latitude, longitude } }) => {
@@ -21,13 +23,13 @@ const App = () => {
 
    useEffect(() => {
      setIsLoading(true);
-     getPlacesData(bounds.sw, bounds.ne)
+     getPlacesData(type, bounds.sw, bounds.ne)
        .then((response) => {
           console.log(response.data);
           setPlaces(response.data);
           setIsLoading(false);
        })
-   }, [coordinates, bounds]);
+   }, [type, coordinates, bounds]);
 
    return (
       <>
@@ -39,6 +41,10 @@ const App = () => {
                   places={places}
                   childClicked={childClicked} 
                   isLoading={isLoading}
+                  type={type}
+                  setType={setType}
+                  rating={rating}
+                  setRating={setRating}
                />
             </Grid>
             <Grid item xs={12} md={8}>
